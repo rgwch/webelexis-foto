1acb8d0d2d36e0a88b73bc9dfed11f0b97352377
gerry <weirich@elexis.ch>
2017-04-11 14:45:54 +0200

    select patient for photo

--- www/js/index.js
+++ www/js/index.js
@@ -8,12 +8,7 @@ var app = {
   usernameField: "ch.webelexis.foto.username",
   passwordField: "ch.webelexis.foto.password",
   urlField: "ch.webelexis.foto.url",
-  selectedPatient:{
-    lastname: "Testperson",
-    firstname:"Armeswesen",
-    birthdate: "01.02.1960",
-    id:"3f4d-4efr-1433-1234"
-  },
+  selectedPatient:"",
 
 // Initilize our app when the device is ready
   initialize: function () {
@@ -43,7 +38,42 @@ var app = {
       $("#server_pwd").val(localStorage.getItem(app.passwordField))
     });
 
-    // When zhe user clicks on "Übernehmen" on the settings screen, transmit values to localStorage
+    // autocomplete search box with patient data
+    $("#autocomplete").on("filterablebeforefilter", function (e, data) {
+      var $ul = $(this),
+          $input = $(data.input),
+          value = $input.val(),
+          html = "";
+      $ul.html("");
+      if (value && value.length > 2) {
+        $ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
+        $ul.listview("refresh");
+        var url = localStorage.getItem(app.urlField) + "/fhir/Patient?name=" + $input.val()
+        $.ajax({
+          url: url,
+          type: "GET",
+          dataType: "json",
+          error: function (err) {
+            alert("Ajax Error: " + JSON.stringify(err))
+          }
+
+        })
+            .then(function (response) {
+              $.each(response.entry, function (i, val) {
+                html += "<li><button class='ui-btn patselect'>" + app.getPersonalia(val) + "</button></li>";
+              });
+              $ul.html(html);
+              $ul.listview("refresh");
+              $('.patselect').click(function(item){
+                app.selectedPatient=item.currentTarget.childNodes[0].data
+                alert("Selected "+app.selectedPatient)
+              })
+              $ul.trigger("updatelayout");
+            })
+      }
+    });
+
+    // When the user clicks on "Übernehmen" on the settings screen, transmit values to localStorage
     $('#saveValues').click(function () {
       var surl = $("#server_url").val()
       localStorage.setItem(app.urlField, surl)
@@ -54,7 +84,7 @@ var app = {
 
   // After camera is finished, POST the picture to the server's REST interface
   saveData: function (data) {
-    var url = localStorage.getItem(app.urlField)+"/addContent/image"
+    var url = localStorage.getItem(app.urlField) + "/addContent/image"
     var uname = localStorage.getItem(app.usernameField)
     var pwd = localStorage.getItem(app.passwordField)
 
@@ -63,7 +93,7 @@ var app = {
       url: url,
       dataType: "json",
       async: false,
-      data: {user: uname, pwd: pwd, patient: selectedPatient, payload: data},
+      data: {user: uname, pwd: pwd, patient: app.selectedPatient, payload: data},
       success: function (val) {
         alert("success")
       },
@@ -84,27 +114,33 @@ var app = {
     console.log('Received Event: ' + id);
   },
 
-  findPatient: function(){
-    var pattern=$('#patname').val()
-    var url=localStorage.getItem(app.urlField)+"/fhir/Patient?name="+pattern
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: "json",
-      async: false,
-      success: function(val){
-        var entries=val.entry
-        if(entries && entries.length>0){
-
+  getPersonalia: function(patEntry){
+    var rsc=patEntry['resource']
+    var lastname="unbekannt"
+    var firstname="unbekannt"
+    var birthdate="unbekannt"
+    if(rsc){
+      var names=rsc['name']
+      if(names && names.length>0){
+        var fam=names[0]['family']
+        if(fam && fam.length>0){
+          lastname=fam[0]
         }
-        alert("success")
-      },
-      error: function(err){
-        alert("Error: "+JSON.stringify(err))
+        var first=names[0]['given']
+        if(first && first.length>0){
+          firstname=first[0]
+        }
       }
-    })
+      var bd=rsc['birthDate']
+      if(bd && bd.length>9){
+        birthdate=bd.substr(8,2)+"."+bd.substr(5,2)+"."+bd.substr(0,4)
+      }
+    }
+    return lastname+" "+firstname+", "+birthdate
   }
+
 };
 
 
 app.initialize();
+
